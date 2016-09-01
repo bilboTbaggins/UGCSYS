ceccd9094fbfcb41d1a329d9b42a08a9bf3804a2
Stephanie <Stephanie@Vespa.gateway>
2016-09-01 13:08:50 +1000

    I fixed the db queries, made the model and fixed the login controller. Untested because I need to update the main method

--- JavaFXApplication12/src/src/Main.java
+++ JavaFXApplication12/src/src/Main.java
@@ -3,37 +3,29 @@
   * and open the template in the editor.
   */
  package src;
- 
- import java.sql.Connection;
- import java.sql.DriverManager;
- import java.sql.ResultSet;
- import java.sql.SQLException;
- import java.sql.Statement;
+
  import javafx.application.Application;
  import javafx.fxml.FXMLLoader;
  import javafx.scene.Parent;
  import javafx.scene.Scene;
  import javafx.stage.Stage;
- 
- import database.DatabaseConnection;
+
  
  /**
   *
   * @author Stephanie
   * 
-  * I made this to start something that looks like a MVC program. The database classes will all be stored in the Database package.
+  * 
   * 
   * The MySQL JDBC driver is setup in the crosspath of this project.
   * 
-  * The main method creates an instance of a Database Connection and then calls the method that I created as an example just to see
-  * if the JDBC was functioning. The method returns a result set from the user table successfully. This method has no place in the final program 
-  * and is only for testing purposes.
+  * 
   */
  public class Main extends Application {
  
      @Override
      public void start(Stage stage) throws Exception {
-         Parent root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+         Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
  
          Scene scene = new Scene(root);
  
@@ -50,9 +42,7 @@
       * @param args the command line arguments
       */
      public static void main(String[] args) {
-         DatabaseConnection databaseConnection = new DatabaseConnection();
-         
-         databaseConnection.openConnection();
+         launch(args);
         
  
      }
